dff2edce0b15fcb1f44379129c5f65a75cfef396
guineveresaenger <guineveresaenger@yahoo.com>
2016-11-21 16:32:27 -0800

    added code for both click and type

--- noise.js
+++ noise.js
@@ -1,3 +1,24 @@
 $(document).ready( function() {
-  // your code here
+
+  $('body').keydown(function(event){
+    var note = event.key;
+    console.log("You clicked on " + note);
+    playNote(note);
+  });
+
+
+  $('.instrument').on('click', 'button', function(event){
+    var note = $(this).html();
+    console.log("You clicked on " + note);
+    playNote(note);
+  });
 });
+
+
+var playNote = function(note) {
+  var keyPlay = $('#' + note + "Audio");
+
+  console.log(keyPlay);
+  $(keyPlay).trigger('play');
+
+};
